7ab2a5dd009d2ce3ebfa3dbf3902ea4355e64297
KellyLaas <laas@iit.edu>
2017-06-14 06:23:33 -0500

    Move files to new repository, change test cases

--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class AuthorTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
   def new_author
     Author.new(first_name: "Peggy",
                last_name: "Connelly")
@@ -8,7 +11,9 @@ class AuthorTest < ActiveSupport::TestCase
   end
 
   test "should not save author without first or last name" do
-    author = Author.new
-    assert_not author.save
+    @author = Author.new
+    assert @author.invalid?
+    assert @author.errors[:first_name].any?
+    assert @author.errors[:last_name].any?
   end
 end
--- test/models/case_test.rb
+++ test/models/case_test.rb
@@ -7,20 +7,12 @@ class CaseTest < ActiveSupport::TestCase
               abstract: "yyyy")
   end
 
-  test "should not save case without a title" do
+  test "should have a title, year and abstract" do
     test_case = Case.new
-    assert_not test_case.save, "Saved case without a title"
+    assert @test_case.invalid?
+    assert @test_case.errors[:title].any?
+    assert @test_case.errors[:year].any?
+    assert @test_case.errors[:abstract].any?
   end
 
-  test "should not save case without a year" do
-    test_case = Case.new(title: "What a day", abstract: "Why me?")
-    refute test_case.valid?, "Case is not valid without a year"
-    assert_not_nil test_case.errors[:year], "no validation error for year present"
-  end
-
-  test "should not save case without a abstract" do
-    test_case = Case.new(title: "What a day", year: 1984)
-    refute test_case.valid?, "Case is not valid without an abstarct"
-    assert_not_nil test_case.errors[:year], "no validation error for abstract present"
-  end
 end
