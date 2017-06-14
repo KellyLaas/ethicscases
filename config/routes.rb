419f5c746bb4b8a16c1c23d74412823f1bb9c089
KellyLaas <laas@iit.edu>
2017-06-14 14:09:55 -0500

    Change routes.rb so root points to case_index

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
-  get 'case/index'
-
-  get 'home/index'
-
-  root 'home#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'case#index', as: 'case_index'
+  # For details on the DSL available within this file, see
+  # http://guides.rubyonrails.org/routing.html
 end
