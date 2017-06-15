82981699b5362988f3e70322590140d7fe990bbf
KellyLaas <laas@iit.edu>
2017-06-15 16:50:43 -0500

    Ran scaffold for cases, throwing up errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  root 'case#index', as: 'case_index'
+  root 'cases#index'
+  resources :cases, :authors, :subjects
+
   # For details on the DSL available within this file, see
   # http://guides.rubyonrails.org/routing.html
 end
